a1815f00e651cce73a58fa1d952e66ba1a49ff13
Urvil Patel <patelurvil38@gmail.com>
2021-12-03 13:03:29 +0530

    Added solution of day 3 puzzles

---
+++ 2021/day03.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+var inputFile = flag.String("inputFile", "inputs/day03.input", "Relative file path to use as input.")
+
+func main() {
+	flag.Parse()
+	input, nl := parseInput()
+	fmt.Println(p1(input, nl))
+	fmt.Println(p2(input, nl))
+}
+
+func p1(input []int, nl int) int {
+	var gRate int
+	for k := nl - 1; k >= 0; k-- {
+		var i0, i1 int
+		for _, v := range input {
+			if (v>>k)&1 == 1 {
+				i1++
+			} else {
+				i0++
+			}
+		}
+
+		nMax := 0
+		if i1 > i0 {
+			nMax = 1
+		}
+		gRate |= (nMax << k)
+	}
+	eRate := ^gRate & ((1 << nl) - 1)
+	return gRate * eRate
+}
+
+func p2(input []int, nl int) int {
+	return rate(input, nl, oxyRate) * rate(input, nl, co2Rate)
+}
+
+type computeRate func(a, b []int) []int
+
+func co2Rate(s0, s1 []int) []int {
+	if len(s0) <= len(s1) {
+		return s0
+	}
+	return s1
+}
+
+func oxyRate(s0, s1 []int) []int {
+	if len(s0) <= len(s1) {
+		return s1
+	}
+	return s0
+}
+
+func rate(input []int, nl int, rateFn computeRate) int {
+	curr := input
+	var s0, s1 []int
+	for k := nl - 1; k >= 0; k-- {
+		if len(curr) == 1 {
+			break
+		}
+		s0 = s0[:0]
+		s1 = s1[:0]
+		for _, v := range curr {
+			if (v>>k)&1 == 1 {
+				s1 = append(s1, v)
+			} else {
+				s0 = append(s0, v)
+			}
+		}
+		curr = rateFn(s0, s1)
+	}
+	return curr[0]
+}
+
+func parseInput() ([]int, int) {
+	b, err := ioutil.ReadFile(*inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	si := strings.Split(string(b), "\n")
+	var input []int
+	for _, v := range si {
+		if v == "" {
+			continue
+		}
+		vi, _ := strconv.ParseInt(v, 2, 64)
+		input = append(input, int(vi))
+	}
+
+	return input, len(si[0])
+}
